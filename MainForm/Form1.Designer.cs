dea730506e5e9891ed9ec3a03c292d23a46157e6
Андрій <caballiero@gmail.com>
2016-03-17 23:56:17 +0200

    Main Commit

---
+++ MainForm/Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace MainForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.MainTimer = new System.Windows.Forms.Timer(this.components);
+            this.Count = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // MainTimer
+            // 
+            this.MainTimer.Interval = 1;
+            // 
+            // Count
+            // 
+            this.Count.AutoSize = true;
+            this.Count.Location = new System.Drawing.Point(12, 530);
+            this.Count.Name = "Count";
+            this.Count.Size = new System.Drawing.Size(0, 13);
+            this.Count.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkGray;
+            this.ClientSize = new System.Drawing.Size(554, 552);
+            this.Controls.Add(this.Count);
+            this.Name = "Form1";
+            this.Text = "Touch Me !!!";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer MainTimer;
+        private System.Windows.Forms.Label Count;
+    }
+}
+
---
+++ MainForm/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MainForm
+{
+    public partial class Form1 : Form
+    {
+        private double R = 250;
+
+        private int r = 50;
+
+        private double angle = 0;
+
+        private Brush b = Brushes.Red;
+
+        private int count = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            MainTimer.Enabled = true;
+
+            MainTimer.Tick += MainTimer_Tick;
+
+            MouseClick += Form1_MouseClick;
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (Math.Sqrt(Math.Pow(e.X - (int)(R * Math.Cos(angle) + R) - r / 2, 2) + Math.Pow(e.Y - (int)(R * Math.Sin(angle) + R) - r / 2, 2)) <= r/2)
+            {
+                if(b == Brushes.Red)
+                {
+                    b = Brushes.Blue;
+                }
+                else
+                {
+                    b = Brushes.Red;
+                }
+                count++;
+                Count.Text = Convert.ToString(count);
+            }
+        }
+
+        private void MainTimer_Tick(object sender, EventArgs e)
+        {
+            Graphics mgr = CreateGraphics();
+
+            mgr.Clear(Color.DarkGray);
+
+            mgr.FillEllipse(b, new Rectangle((int)(R * Math.Cos(angle) + R), (int)(R * Math.Sin(angle) + R), r, r));
+
+            angle += Math.PI / 180;
+
+            if(angle == Math.PI * 2)
+
+            {
+                angle = 0;
+            }
+
+        }
+    }
+}
